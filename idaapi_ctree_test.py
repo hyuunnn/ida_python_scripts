cc50819a4efd39790dc4ae451f12774e39f0de15
Hyun Yi <hy00un__@naver.com>
2018-04-13 09:29:37 +0900

    Upload

---
+++ idaapi_ctree_test.py
@@ -0,0 +1,2 @@
+# https://www.hex-rays.com/products/ida/support/idapython_docs/ida_hexrays.ctree_visitor_t-class.html
+# https://github.com/idapython/src/blob/master/examples/vds3.py
---
+++ simplecustviewer_test.py
@@ -0,0 +1,5 @@
+# http://www.hexblog.com/?p=119
+# https://github.com/idapython/src/tree/master/examples/ex_custview.py
+# https://www.hex-rays.com/products/ida/support/idapython_docs/ida_kernwin.simplecustviewer_t-class.html
+
+from idaapi import simplecustviewer_t
