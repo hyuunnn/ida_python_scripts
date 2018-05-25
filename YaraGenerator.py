1480f0639304d27d53f2770a2186b2c69a91282d
Hyun Yi <hy00un__@naver.com>
2018-05-25 23:19:06 +0900

    Update YaraChecker

--- YaraGenerator.py
+++ YaraGenerator.py
@@ -1,11 +1,77 @@
-
 from idaapi import PluginForm
-from PyQt5.QtWidgets import QTableWidgetItem, QTableWidget, QLineEdit, QPlainTextEdit, QPushButton, QLabel, QVBoxLayout, QGridLayout
+from PyQt5.QtWidgets import QTableWidgetItem, QFileDialog, QTableWidget, QLineEdit, QPlainTextEdit, QPushButton, QLabel, QVBoxLayout, QGridLayout
 from PyQt5.QtGui import QColor
 from PyQt5.QtCore import Qt
+from os.path import expanduser
+
+import os
+import yara
+
+class YaraChecker(PluginForm):
+    def choose_path(self):
+        path = QFileDialog.getExistingDirectory(
+            self.parent,
+            "Open a folder",
+            expanduser("~"),
+            QFileDialog.ShowDirsOnly)
+        self.path.setText(path)
+
+    def Search(self):
+        rule = yara.compile(source=self.TextEdit1.toPlainText())
+        result = {}
+        for i in os.walk(self.path.text()):
+            for j in i[2]:
+                f = open(i[0] + "\\" + j, "rb")
+                data = f.read()
+                matches = rule.match(data=data)
+                f.close()
+                for match in matches:
+                    strings = match.strings[0]
+                    result[os.path.basename(j)] = [i[0], hex(strings[0]).replace("L",""), strings[1], strings[2]]
+        self.tableWidget.setRowCount(len(result.keys()))
+        
+        for idx, filename in enumerate(result.keys()):
+            self.tableWidget.setItem(idx, 0, QTableWidgetItem(result[filename][0]))
+            self.tableWidget.setItem(idx, 1, QTableWidgetItem(filename))
+            self.tableWidget.setItem(idx, 2, QTableWidgetItem(result[filename][1]))
+            self.tableWidget.setItem(idx, 3, QTableWidgetItem(result[filename][2]))
+            self.tableWidget.setItem(idx, 4, QTableWidgetItem(result[filename][3]))
+        self.layout.addWidget(self.tableWidget)
+
+    def OnCreate(self, form):
+        self.parent = self.FormToPyQtWidget(form)
+        self.label1 = QLabel("Search Path")
+        self.path = QLineEdit()
+        self.PathButton = QPushButton("path")
+        self.PathButton.clicked.connect(self.choose_path)        
+        self.label2 = QLabel("Yara rule")
+        self.TextEdit1 = QPlainTextEdit()
+        self.TextEdit1.insertPlainText(self.data)
+        self.SearchButton = QPushButton("Search")
+        self.SearchButton.clicked.connect(self.Search)
+
+        self.layout = QVBoxLayout()
+        GL1 = QGridLayout()
+        GL1.addWidget(self.path, 0, 0)
+        GL1.addWidget(self.PathButton, 0, 1)
+        self.layout.addLayout(GL1)
+
+        self.layout.addWidget(self.label2)
+        self.layout.addWidget(self.TextEdit1)
+        self.layout.addWidget(self.SearchButton)
+
+        self.tableWidget = QTableWidget()
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setColumnCount(5)
+        self.tableWidget.setHorizontalHeaderLabels(["Path", "Filename", "Address", "Variable_name", "String"])
+        self.layout.addWidget(self.tableWidget)
+        self.parent.setLayout(self.layout)
+
+    def OnClose(self, form):
+        pass
+
 
 class YaraGenerator(PluginForm):
-
     def YaraExport(self):
         result = ""
         result += "rule " + self.Variable_name.text() + "\n{\n"
@@ -20,7 +86,8 @@ class YaraGenerator(PluginForm):
         self.TextEdit1.insertPlainText(result)
 
     def DeleteRule(self):
-        self.ruleset_list = {}
+        if idaapi.ask_yn(idaapi.ASKBTN_NO, "Delete Yara Rule"):
+            self.ruleset_list = {}
         self.tableWidget.setRowCount(len(self.ruleset_list.keys()))
         self.tableWidget.setColumnCount(4)
         self.tableWidget.setHorizontalHeaderLabels(["Variable_name", "Rule", "Start", "End"])
@@ -30,7 +97,6 @@ class YaraGenerator(PluginForm):
             self.tableWidget.setItem(idx, 2, QTableWidgetItem(self.ruleset_list[name][1]))
             self.tableWidget.setItem(idx, 3, QTableWidgetItem(self.ruleset_list[name][2]))
         self.layout.addWidget(self.tableWidget)
-
 
     def MakeRule(self):
         ByteCode = []
@@ -58,6 +124,11 @@ class YaraGenerator(PluginForm):
             self.tableWidget.setItem(idx, 3, QTableWidgetItem(self.ruleset_list[name][2]))
         self.layout.addWidget(self.tableWidget)
 
+    def YaraChecker(self):
+        self.YaraChecker = YaraChecker()
+        self.YaraChecker.data = self.TextEdit1.toPlainText()
+        self.YaraChecker.Show("YaraChecker")
+
     def OnCreate(self, form):
         self.parent = self.FormToPyQtWidget(form)
         self.ruleset_list = {}
@@ -77,6 +148,8 @@ class YaraGenerator(PluginForm):
         self.DeleteButton.clicked.connect(self.DeleteRule)
         self.YaraExportButton = QPushButton("Export Yara Rule")
         self.YaraExportButton.clicked.connect(self.YaraExport)
+        self.YaraCheckerButton = QPushButton("Yara Checker")
+        self.YaraCheckerButton.clicked.connect(self.YaraChecker)
 
         self.layout = QVBoxLayout()
 
@@ -99,17 +172,13 @@ class YaraGenerator(PluginForm):
         GL3.addWidget(self.SaveButton, 0, 1)
         GL3.addWidget(self.DeleteButton, 0, 2)
         GL3.addWidget(self.YaraExportButton, 0, 3)
+        GL3.addWidget(self.YaraCheckerButton, 0, 4)
         self.layout.addLayout(GL3)
 
         self.tableWidget = QTableWidget()
-        self.tableWidget.setRowCount(len(self.ruleset_list.keys()))
+        self.tableWidget.setRowCount(0)
         self.tableWidget.setColumnCount(4)
         self.tableWidget.setHorizontalHeaderLabels(["Variable_name", "Rule", "Start", "End"])
-        for idx, name in enumerate(self.ruleset_list.keys()):
-            self.tableWidget.setItem(idx, 0, QTableWidgetItem(name))
-            self.tableWidget.setItem(idx, 1, QTableWidgetItem(self.ruleset_list[name][0]))
-            self.tableWidget.setItem(idx, 2, QTableWidgetItem(self.ruleset_list[name][1]))
-            self.tableWidget.setItem(idx, 3, QTableWidgetItem(self.ruleset_list[name][2]))
         self.layout.addWidget(self.tableWidget)
 
         self.parent.setLayout(self.layout)
