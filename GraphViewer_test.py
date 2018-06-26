3b4959c1dcb8701de45a647059d9da863392d2ad
Hyun Yi <hy00un__@naver.com>
2018-06-26 20:49:07 +0900

    delete file

--- GraphViewer_test.py
+++
@@ -1,5 +0,0 @@
-# https://github.com/idapython/src/tree/master/examples/ex_graph.py
-# https://www.hex-rays.com/products/ida/support/idapython_docs/ida_graph.GraphViewer-class.html
-
-from idaapi import *
-
