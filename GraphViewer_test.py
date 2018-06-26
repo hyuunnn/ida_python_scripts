5401557ca1b732e0056f0bc43b0f3b2c1ab3e3a4
Hyun Yi <hy00un__@naver.com>
2018-06-26 20:50:15 +0900

    GraphViewer test

---
+++ GraphViewer_test.py
@@ -0,0 +1,5 @@
+# https://github.com/idapython/src/tree/master/examples/ex_graph.py
+# https://www.hex-rays.com/products/ida/support/idapython_docs/ida_graph.GraphViewer-class.html
+
+from idaapi import *
+
